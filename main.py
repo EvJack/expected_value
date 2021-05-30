fcc02358c356c24855d548e9a215f6c918342f02
Eugene <shelowek@yandex.ru>
2021-05-30 12:13:48 +0700

    Updare main.py

--- main.py
+++ main.py
@@ -7,7 +7,27 @@ import plotly.graph_objs as go
 startmoney = int(input("Введите ваш стартовый баланс: "))
 
 COEFFICIENT = 0.001
-WIN_FIELDS = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 28, 29, 31, 33, 35]
+WIN_FIELDS = [
+    2,
+    4,
+    6,
+    8,
+    10,
+    11,
+    13,
+    15,
+    17,
+    20,
+    22,
+    24,
+    26,
+    28,
+    28,
+    29,
+    31,
+    33,
+    35]
+
 STOP_POINT = 40000
 
 balance1 = []
@@ -49,10 +69,11 @@ def strategy1():
             loose += 1
     games = win + loose
     print("Стратегия №1",
-        f"Общее цисло игр: {games}", 
-        f"Выиграно ставок: {str(win)}, ({str(win/games * 100)}%)", 
-        f"Проиграно ставок: {str(loose)}, ({str(loose/games * 100)}%)", 
+        f"Общее цисло игр: {games}",
+        f"Выиграно ставок: {str(win)}, ({str(win/games * 100)}%)",
+        f"Проиграно ставок: {str(loose)}, ({str(loose/games * 100)}%)",
         sep="\n", end="\n\n")
+
 
 def strategy2():
     win = 0
@@ -76,9 +97,10 @@ def strategy2():
     games = win + loose
     print("Стратегия №2",
         f"Общее цисло игр: {games}",
-        f"Выиграно ставок: {str(win)}, ({str(win/games * 100)}%)", 
-        f"Проиграно ставок: {str(loose)}, ({str(loose/games * 100)}%)", 
+        f"Выиграно ставок: {str(win)}, ({str(win/games * 100)}%)",
+        f"Проиграно ставок: {str(loose)}, ({str(loose/games * 100)}%)",
         sep="\n", end="\n\n")
+
 
 def strategy3():
     win = 0
@@ -100,48 +122,53 @@ def strategy3():
         else:
             loose += 1
     games = win + loose
-    print("Стратегия №3", 
+    print("Стратегия №3",
         f"Общее цисло игр: {games}",
-        f"Выиграно ставок: {str(win)}, ({str(win/games * 100)}%)", 
-        f"Проиграно ставок: {str(loose)}, ({str(loose/games * 100)}%)", 
+        f"Выиграно ставок: {str(win)}, ({str(win/games * 100)}%)",
+        f"Проиграно ставок: {str(loose)}, ({str(loose/games * 100)}%)",
         sep="\n", end="\n\n")
 
+
 def graph():
-    question_graph = input("Хотите ли вы построить графики изменения баланса? [y/n]: ")
+    question_graph = input(
+        "Хотите ли вы построить графики изменения баланса? [y/n]: ")
 
     if question_graph == "y":
         fig = go.Figure()
-        fig.add_trace(go.Scatter(x=games1, y=balance1, name = "Отрицательное матожидание"))
-        fig.add_trace(go.Scatter(x=games2, y=balance2, name = "Нулевое матожидание"))
-        fig.add_trace(go.Scatter(x=games3, y=balance3, name = "Положительное матожидание"))
+        fig.add_trace(
+            go.Scatter(
+                x=games1,
+                y=balance1,
+                name="Отрицательное матожидание"))
+        fig.add_trace(
+            go.Scatter(
+                x=games2,
+                y=balance2,
+                name="Нулевое матожидание"))
+        fig.add_trace(
+            go.Scatter(
+                x=games3,
+                y=balance3,
+                name="Положительное матожидание"))
 
         fig.update_layout(legend_orientation="h",
-        legend=dict(x=.5, xanchor="center"),
-        title="Изменение баланса с течением времени",
-        xaxis_title="Кол-во игр",
-        yaxis_title="Баланс",
-        margin=dict(l=0, r=0, t=50, b=100)),
-        fig.update_traces(hoverinfo="all", hovertemplate="Игра: %{x}<br>Баланс: %{y}")
-        
-        fig.show()
+                        legend=dict(x=.5, xanchor="center"),
+                        title="Изменение баланса с течением времени",
+                        xaxis_title="Кол-во игр",
+                        yaxis_title="Баланс",
+                        margin=dict(l=0, r=0, t=50, b=100)),
+        fig.update_traces(
+            hoverinfo="all",
+            hovertemplate="Игра: %{x}<br>Баланс: %{y}")
 
-        savedata()
-    else:
-        savedata()
-    
-    question_image = input("Хотите ли вы сохранить изображение графика? [y/n]: ")
-    if question_image == "y":
-        if not os.path.exists("result"):
-            os.mkdir("result")
-        fig.write_image("result/graph.png")
-    else:
-        savedata()
+        fig.show()
 
 
 def savedata():
 
-    question = input("Хотите ли вы сохранить Excel-таблицу с результатами? [y/n]: ")
-    
+    question = input(
+        "Хотите ли вы сохранить Excel-таблицу с результатами? [y/n]: ")
+
     if question == "y":
         arr_games1 = np.array(games1)
         arr_balance1 = np.array(balance1)
@@ -150,29 +177,31 @@ def savedata():
         arr_games3 = np.array(games3)
         arr_balance3 = np.array(balance3)
 
-        data = {'Кол-во игр 1':arr_games1,
-                'Баланс по стратегии 1':arr_balance1,
-                'Кол-во игр 2':arr_games2,
-                'Баланс по стратегии 2':arr_balance2,
-                'Кол-во игр 3':arr_games3,
-                'Баланс по стратегии 3':arr_balance3,
+        data = {'Кол-во игр 1': arr_games1,
+                'Баланс по стратегии 1': arr_balance1,
+                'Кол-во игр 2': arr_games2,
+                'Баланс по стратегии 2': arr_balance2,
+                'Кол-во игр 3': arr_games3,
+                'Баланс по стратегии 3': arr_balance3,
                 }
 
         df = pd.DataFrame.from_dict(data, orient="index")
         df = df.transpose()
-        print(df)
+        # print(df)
         if not os.path.exists("result"):
             os.mkdir("result")
         df.to_excel("result/result.xlsx")
+
 
 def main():
     strategy1()
     strategy2()
     strategy3()
     graph()
+    savedata()
+
 
 if __name__ == "__main__":
     main()
 
-print("Для выхода нажмите на любую клавишу...")
-os.system("pause")
+input("Для выхода нажмите Enter ...")

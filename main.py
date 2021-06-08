faa19e032589dd89fbd54f57da2aba8fddbfd5b9
Eugene <shelowek@yandex.ru>
2021-06-08 23:07:58 +0700

    updated according to google styleguide

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import pandas as pd
 import plotly.graph_objs as plotly_go
 from configuration import BET_COEFFICIENT, STOP_POINT, WIN_FIELDS
 
+
 def balance_manipulaion(balance, BET):
     # balance -= BET if BET < balance else 0
     if BET > balance:
@@ -23,12 +24,16 @@ def check_result(balance, win, loose, BET, start_value, end_value):
         loose += 1
     return balance, win, loose
 
+
 def print_result(index, win, loose):
-    print(f"Стратегия №{index}",
+    print(
+        f"Стратегия №{index}",
         f"Общее число игр: {win + loose}",
         f"Выиграно ставок: {str(win)}, ({str(win/(win + loose) * 100)}%)",
         f"Проиграно ставок: {str(loose)}, ({str(loose/(win + loose) * 100)}%)",
-        sep="\n", end="\n\n")
+        sep="\n",
+        end="\n\n")
+
 
 def game_strategy_1(balance, BET, balance_strategy_1):
     win = 0
@@ -43,6 +48,7 @@ def game_strategy_1(balance, BET, balance_strategy_1):
     print_result(1, win, loose)
     return balance_strategy_1
 
+
 def game_strategy_2(balance, BET, balance_strategy_2):
     win = 0
     loose = 0
@@ -55,6 +61,7 @@ def game_strategy_2(balance, BET, balance_strategy_2):
 
     print_result(2, win, loose)
     return balance_strategy_2
+
 
 def game_strategy_3(balance, BET, balance_strategy_3):
     win = 0
@@ -77,41 +84,40 @@ def count_games(balance_strategy_1, balance_strategy_2, balance_strategy_3):
 
     return count_games_strategy_1, count_games_strategy_2, count_games_strategy_3
 
+
 def graph(balance_strategy_1, balance_strategy_2, balance_strategy_3):
     question_graph = input(
         "Хотите ли вы построить графики изменения баланса? [y/n]: ")
 
     if question_graph == "y":
 
-        count_games_strategy_1, count_games_strategy_2, count_games_strategy_3 = count_games(balance_strategy_1, balance_strategy_2, balance_strategy_3)
+        count_games_strategy_1, count_games_strategy_2, count_games_strategy_3 = count_games(
+            balance_strategy_1, balance_strategy_2, balance_strategy_3)
         figure = plotly_go.Figure()
         figure.add_trace(
-            plotly_go.Scatter(
-                x=count_games_strategy_1,
-                y=balance_strategy_1,
-                name="Отрицательное матожидание"))
+            plotly_go.Scatter(x=count_games_strategy_1,
+                              y=balance_strategy_1,
+                              name="Отрицательное матожидание"))
         figure.add_trace(
-            plotly_go.Scatter(
-                x=count_games_strategy_2,
-                y=balance_strategy_2,
-                name="Нулевое матожидание"))
+            plotly_go.Scatter(x=count_games_strategy_2,
+                              y=balance_strategy_2,
+                              name="Нулевое матожидание"))
         figure.add_trace(
-            plotly_go.Scatter(
-                x=count_games_strategy_3,
-                y=balance_strategy_3,
-                name="Положительное матожидание"))
+            plotly_go.Scatter(x=count_games_strategy_3,
+                              y=balance_strategy_3,
+                              name="Положительное матожидание"))
 
         figure.update_layout(legend_orientation="h",
-                        legend=dict(x=.5, xanchor="center"),
-                        title="Изменение баланса с течением времени",
-                        xaxis_title="Кол-во игр",
-                        yaxis_title="Баланс",
-                        margin=dict(l=0, r=0, t=50, b=100)),
-        figure.update_traces(
-            hoverinfo="all",
-            hovertemplate="Игра: %{x}<br>Баланс: %{y}")
+                             legend=dict(x=.5, xanchor="center"),
+                             title="Изменение баланса с течением времени",
+                             xaxis_title="Кол-во игр",
+                             yaxis_title="Баланс",
+                             margin=dict(l=0, r=0, t=50, b=100)),
+        figure.update_traces(hoverinfo="all",
+                             hovertemplate="Игра: %{x}<br>Баланс: %{y}")
 
         figure.show()
+
 
 def savedata(balance_strategy_1, balance_strategy_2, balance_strategy_3):
 
@@ -120,7 +126,8 @@ def savedata(balance_strategy_1, balance_strategy_2, balance_strategy_3):
 
     if question == "y":
 
-        count_games_strategy_1, count_games_strategy_2, count_games_strategy_3 = count_games(balance_strategy_1, balance_strategy_2, balance_strategy_3)
+        count_games_strategy_1, count_games_strategy_2, count_games_strategy_3 = count_games(
+            balance_strategy_1, balance_strategy_2, balance_strategy_3)
 
         arr_count_games_strategy_1 = np.array(count_games_strategy_1)
         arr_balance_strategy_1 = np.array(balance_strategy_1)
@@ -129,15 +136,14 @@ def savedata(balance_strategy_1, balance_strategy_2, balance_strategy_3):
         arr_count_games_strategy_3 = np.array(count_games_strategy_3)
         arr_balance_strategy_3 = np.array(balance_strategy_3)
 
-        data = {'Кол-во игр 1': arr_count_games_strategy_1,
-                'Баланс по стратегии 1': arr_balance_strategy_1,
-                
-                'Кол-во игр 2': arr_count_games_strategy_2,
-                'Баланс по стратегии 2': arr_balance_strategy_2,
-                
-                'Кол-во игр 3': arr_count_games_strategy_3,
-                'Баланс по стратегии 3': arr_balance_strategy_3,
-                }
+        data = {
+            'Кол-во игр 1': arr_count_games_strategy_1,
+            'Баланс по стратегии 1': arr_balance_strategy_1,
+            'Кол-во игр 2': arr_count_games_strategy_2,
+            'Баланс по стратегии 2': arr_balance_strategy_2,
+            'Кол-во игр 3': arr_count_games_strategy_3,
+            'Баланс по стратегии 3': arr_balance_strategy_3,
+        }
 
         df = pd.DataFrame.from_dict(data, orient="index")
         df = df.transpose()
@@ -146,17 +152,16 @@ def savedata(balance_strategy_1, balance_strategy_2, balance_strategy_3):
             os.mkdir("result")
         df.to_excel("result/result.xlsx")
 
-    
 
 def main():
     balance = int(input("Введите ваш стартовый баланс: "))
-    
+
     BET = balance * BET_COEFFICIENT
 
     balance_strategy_1 = [balance]
     balance_strategy_2 = [balance]
     balance_strategy_3 = [balance]
-    
+
     game_strategy_1(balance, BET, balance_strategy_1)
     game_strategy_2(balance, BET, balance_strategy_2)
     game_strategy_3(balance, BET, balance_strategy_3)
